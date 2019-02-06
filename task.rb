07646753bd45d527b1ac631cbf30e67d3426ed61
vignesh <vigneshit13@gmail.com>
2019-02-06 19:46:22 +0530

    Ruby Basics

--- task.rb
+++ task.rb
@@ -25,12 +25,28 @@ if s1.equal?(s2)
 else
   puts "NotEqual"
 end
+#Conditional Operator
+(s1==s2)?(puts "true"):(puts" false")
+
+locations=%w{chennai delhi coimbatore mumbai punjab}
+locations.delete('delhi')
+
+locations.each do |loc|
+  puts "I love "+loc+'!'
+  puts "Dont YOu?"
 
 
+a,b,c,d=1,2,3
+  puts a,b,c,d
 
+end
 
+digits=-1..9
+puts digits.include?(5)
+puts digits.min
+puts digits.max
+puts digits.reject { |i| i<5}
 
+res= :yes
 
-
-
-
+(res == 'yes')?(puts "Yes"):(puts "No")
