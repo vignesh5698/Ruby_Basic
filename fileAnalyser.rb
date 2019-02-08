d5cc444accc545315b587bf083e95da2913b2fa6
vignesh <vigneshit13@gmail.com>
2019-02-08 16:52:43 +0530

    Ruby Basics

--- fileAnalyser.rb
+++ fileAnalyser.rb
@@ -3,12 +3,15 @@ File.open("doc.txt").each do |line|
   $sentence=$str.split(".")
   $sentence_length=$sentence.length
   $tot_words=0
-  puts "---------------Document Analyser---------------"
-  puts "Total number of sentences in given passage : #$sentence_length "
+  puts "\n\n---------------Document Analyser---------------"
+  puts "\nTotal number of sentences in given passage : #$sentence_length "
+if $sentence_length>0
+
   puts "Number of Words in : "
   $i=0
   arr=Array.new
   letters=Array.new
+  avg_letter=Array.new
   while $i<=$sentence_length-1
     $words=$sentence[$i].split()
     $w_len=$words.length
@@ -18,14 +21,36 @@ File.open("doc.txt").each do |line|
     puts "Sentence #$i : #$w_len"
   end
   puts "Total number of words in given passage : #$tot_words "
-  puts $str.length
+  puts "\nNumber of letters in :"
+
   #Number of letters in each sentence
+
   $j=0
-  while $j<$sentence_length-1
+  $n=0
+  while $j<=$sentence_length-1
     $len=0
     $words=$sentence[$j].split()
-    $len=$len+$words[$j].length
+    $w_len=$words.length
+    $k=0
+    while $k<$w_len
+      $len=$len+$words[$k].length
+      $k+=1
+    end
+    letters[$n]=$len
+    avg_letter[$j]=$len/$w_len
     $j+=1
+    puts "Sentence #$j : #$len"
+  end
+  $tot_letters=$str.length
+  puts "Total number of letters in given passage : #$tot_letters "
+
+  puts "\n\nAverage number of letters in : "
+  $m=0
+  while $m <= $sentence_length-1
+    $x=avg_letter[$m]
+    $m+=1
+    puts "Sentence #$m : #$x"
   end
 
+end
 end
